9be124015fa145220ca427d2e40190144f0d8c71
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-09-21 13:55:19 -0300

    Exercício Completo

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("É que esses cara imitam os gringo");
+Thread.Sleep(2000);
+
+Console.WriteLine("Eu sou brasileiro");
+Thread.Sleep(2000);
+
+Console.WriteLine("Funkeiro nato, coração e alma de maloqueiro");
+Thread.Sleep(2000);
+
+Console.WriteLine("Se tiver palco nós toma de assalto e faz girar os paco");
+Thread.Sleep(2000);
+
+Console.WriteLine("DJ Boy que 'tá no beat, segura o rasteiro, ê");
+Thread.Sleep(2000);
+
+Console.WriteLine("Parceiro bola uma trave, que hoje tem o coringão na tv");
+Thread.Sleep(1000);
+
+Console.WriteLine("Lembra da positividade, que hoje em dia é difícil de ver");
+Thread.Sleep(1000);
+
+Console.WriteLine("Grana não é vaidade, então vai na moral pra não se entorpecer");
+Thread.Sleep(1000);
+
+Console.WriteLine("Do que não tem necessidade ae");
+Thread.Sleep(1000);
+
+Console.WriteLine("O FIM É TRISTE");
+Thread.Sleep(1000);
+
+
+
+
+
+
